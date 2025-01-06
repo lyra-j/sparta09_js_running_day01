54839ec7f77f22e44c7d4c289dc9bfffd9fc3d94
lyra <mk.lyra.j@gmail.com>
2025-01-06 18:33:33 +0900

    fix: 06 설명 수정

--- 06.js
+++ 06.js
@@ -23,9 +23,10 @@ console.log("A", userACart);
 console.log("B", userBCart);
 
 // 1.	실행 결과로 userACart.items와 userBCart.items는 각각 어떻게 달라져 있을까요?
-// -> const userBCart = userACart 로 인해서 userB의카트에 userA카트를 바로 할당해 주었기에 동일한 메모리를
+// -> userACart와 userBCart 모두 각 아이템들 가격이 5,000원 씩 할인된 가격으로 표기됨
 
 // 2.	1번의 결과에 대한 이유를 설명해보세요.
+// -> const userBCart = userACart 로 인해서 userBCart에 uuserACart를 바로 할당해 주었기에 동일한 메모리를주소를 바라보기 떄문.
 
 // 3.	원래 의도대로라면 유저 A와 유저 B 장바구니가 독립적으로 동작해야 하는데, 그렇게 하려면 코드를 어떻게 수정해야 할까요?
 // -> 깊은 복사하여 userBCart와 userACart가 다른 메모리주소를 바라 보도록 만들기
